d3bf22a19d71db4b7ad94321b1d1a12ad2a06c33
Collins Mugarura <collismugy@hotmail.com>
2019-05-17 00:57:44 +0100

    added styles

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../models/product';
+import {ActivatedRoute} from '@angular/router';
+import {ProductService} from '../shared/product.service';
 
 @Component({
   selector: 'nga-product-detail',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+  }
 
   ngOnInit() {
+
+    const prodId: number = parseInt(this.route.snapshot.params['productId'], 10);
+    this.product = this.productService.getProductById(prodId);
   }
 
 }
